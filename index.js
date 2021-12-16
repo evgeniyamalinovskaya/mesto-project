adb6a3d4e255eeded3eb765db308464398e48ea5
evgeniyamalinovskaya <malinovskayaevgeniya@yandex.com>
2021-12-16 16:50:38 +0500

    add edit profile

--- index.js
+++ index.js
@@ -27,101 +27,108 @@ const initialCards = [
 ];
 
 // Кнопки редактирование профиля
-const editProfilePopup = document.getElementById('popup-edit-profile');
+const editProfilePopup = document.querySelector('.popup_profile');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileCloseButton = editProfilePopup.querySelector('.popup__close-button');
-
-// Кнопки добавление карточек
-const addCardPopup = document.getElementById('popup-add-card');
-const showAddCardPopup = document.querySelector('.profile__add-button');
-const cardCloseButton = addCardPopup.querySelector('.popup__close-button');
-
-// Увеличение картинки
-const popupImage = document.getElementById('popup-image');
-const imageCloseButton = popupImage.querySelector('.popup__close-button');
-
+// Получаем имя и профессию редактирование профиля
+const profileTitle = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
 // Форму в DOM редактирования профиля
 const formElement = document.querySelector('form[name="form-info"]');
+// Находим поля формы в DOM редактирования профиля
+const nameInput = formElement.querySelector('input[name="username"]');
+const jobInput = formElement.querySelector('input[name="about"]');
 
+// Кнопки добавление карточек
+const addCardPopup = document.querySelector('.popup_card');
+const showAddCardPopup = document.querySelector('.profile__add-button');
+const cardCloseButton = addCardPopup.querySelector('.popup__close-button');
 // Форму в DOM добавления карточки
 const formCard = document.querySelector('form[name="form-card"]');
 
-//
+// Увеличение картинки
+const imagePopup = document.querySelector('.popup_image');
+const imageCloseButton = imagePopup.querySelector('.popup__close-button');
+
+
+// Находим поля формы в DOM добавления карточки
 const cardTemplate = document.querySelector('#card-template').content;
 const cardContainer = document.querySelector('.elements__list');
 
+//  Находим поля формы в DOM увеличения карточки
+const zoomedImagePopup = document.querySelector('.popup__image');
+const imageText = document.querySelector('.popup__caption');
 
 // Добавить стандартные карточки
-initialCards.forEach(function (card) {
-    addCard(card.name, card.link);
+const standartCards = initialCards.map(function (card) {
+    return createCard(card.name, card.link);
 });
+cardContainer.prepend(...standartCards);
 
 // Открытие формы, изменение данных профиля
 profileEditButton.addEventListener('click', editProfile);
 
+formElement.addEventListener('submit', saveProfileForm);
+
 function editProfile() {
-    // 1. Открываем popup
-    toggleEditProfilePopup(editProfilePopup);
+    // Открываем popup
+    openPopup(editProfilePopup);
 
-    formElement.addEventListener('submit', formProfileSave);
-
-    // 2. Получаем имя и профессию
-    let profileTitle = document.querySelector('.profile__title');
-    let profileJob = document.querySelector('.profile__subtitle');
-
-    // 3. Находим поля формы в DOM
-    let nameInput = formElement.querySelector('input[name="username"]');
-    let jobInput = formElement.querySelector('input[name="about"]');
-
-    // 4. Подставляем значения из профиля
+    // Подставляем значения из профиля
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileJob.textContent;
-
-    function formProfileSave(evt) {
-        evt.preventDefault();
-
-        // 5.Вставляем новые значения
-        profileTitle.textContent = nameInput.value;
-        profileJob.textContent = jobInput.value;
-
-        // 6. Сохранение данных
-        toggleEditProfilePopup(editProfilePopup);
-    }
 }
-// Функция открытия/закрытия popup профиля
-profileCloseButton.addEventListener('click', () => toggleEditProfilePopup(editProfilePopup));
-function toggleEditProfilePopup(editProfilePopup) {
-    editProfilePopup.classList.toggle('popup_opened');
+
+function saveProfileForm(evt) {
+    evt.preventDefault();
+
+    // Вставляем новые значения
+    profileTitle.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
+    // Сохранение данных
+    closePopup(editProfilePopup);
+}
+
+// Функция закрытия popup профиля
+profileCloseButton.addEventListener('click', () => closePopup(editProfilePopup));
+
+// Универсальная функция открытия всех popup
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
+}
+
+// Универсальная функция закрытия всех popup
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
 }
 
 // Добавление карточки
 showAddCardPopup.addEventListener('click', function () {
     clearCardForm();
-    toggleCardPopup(addCardPopup);
+    openPopup(addCardPopup);
 });
 
-
-formCard.addEventListener('submit', formCardAdd);
+formCard.addEventListener('submit', saveCardForm);
 
 // 2. Находим поля формы в DOM
-let nameInput = formCard.querySelector('input[name="name"]');
+let placeInput = formCard.querySelector('input[name="name"]');
 let linkInput = formCard.querySelector('input[name="link"]');
 
 // Функция сохранения карточки
-function formCardAdd(evt) {
+function saveCardForm(evt) {
     evt.preventDefault();
 
-    addCard(nameInput.value, linkInput.value);
+    createCard(placeInput.value, linkInput.value);
 
     // 3. Сохранение данных
-    toggleCardPopup(addCardPopup);
+    closePopup(addCardPopup);
 
     // 4. Очищаем форму карточки
     clearCardForm();
 }
-
 // Функция добавления карточки в поля формы
-function addCard(nameValue, linkValue) {
+function createCard(placeValue, linkValue) {
     let card = cardTemplate.querySelector('.elements__card').cloneNode(true);
     let cardImage = card.querySelector('.elements__image');
     let cardText = card.querySelector('.elements__title');
@@ -130,53 +137,43 @@ function addCard(nameValue, linkValue) {
 
     // Подставить данные из объекта
     cardImage.src = linkValue;
-    cardImage.alt = nameValue;
-    cardText.textContent = nameValue;
-
-    cardContainer.prepend(card);
+    cardImage.alt = placeValue;
+    cardText.textContent = placeValue;
 
     // Добавление лайка карточке
     cardLike.addEventListener('click', function (evt) {
-        evt.target.classList.toggle('elements__like_active');
+    evt.target.classList.toggle('elements__like_active');
     });
 
     //Удаление карточки
-    cardRemove.addEventListener('click', function (evt) {
-        evt.target.parentElement.remove();
+    cardRemove.addEventListener('click',function (evt) {
+    evt.target.closest('.elements__card').remove();
     });
 
     // Открытие Картинки на карточках
-    cardImage.addEventListener('click', image);
+    cardImage.addEventListener('click', zoomImagePopup);
 
-    function image() {
+    function zoomImagePopup() {
         // Открываем popup
-        togglePopupImage(popupImage);
-
-        //  Находим поля формы в DOM
-        let image  = document.querySelector('.popup__image');
-        let imageText = document.querySelector('.popup__caption');
+        openPopup(imagePopup);
 
         // Подставляем данные из объекта
-        image.src  = linkValue;
-        imageText.textContent = nameValue;
+        zoomedImagePopup.src = linkValue;
+        zoomedImagePopup.alt = placeValue;
+        imageText.textContent = placeValue;
     }
+    return card;
 }
 
-// Функция открытия/закрытия popup увеличение карточек
-imageCloseButton.addEventListener('click', () => togglePopupImage(popupImage));
-function togglePopupImage(popupImage) {
-    popupImage.classList.toggle('popup_image-opened');
-}
+// Функция закрытия popup увеличение карточек
+imageCloseButton.addEventListener('click', () => closePopup(imagePopup));
 
-// Функция открытия/закрытия popup добавления карточек
-cardCloseButton.addEventListener('click', () => toggleCardPopup(addCardPopup));
-function toggleCardPopup(cardPopup) {
-    cardPopup.classList.toggle('popup_opened');
-}
+// Функция закрытия popup добавления карточек
+cardCloseButton.addEventListener('click', () => closePopup(addCardPopup));
 
 // Функция очистки поля в форме карточки
 function clearCardForm() {
-    nameInput.value = "";
+    placeInput.value = "";
     linkInput.value = "";
-    console.log(nameInput);
+    console.log(placeInput);
 }
